29d49476851a957421bf52932df3333648ae9909
Aymeric Mathéossian <aymeric.matheossian@artifakt.io>
2021-05-05 16:56:12 +0200

    Edit index.php

--- index.php
+++ index.php
@@ -13,27 +13,27 @@
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/custom.css">
+    <link rel="stylesheet" href="/assets/css/custom.css">
   </head>
   <body>
     <main>
       
       <div class="container mt-4">
         <div class="mb-5">
-          <img src="assets/img/Artifakt-Logo.svg" style="width:140px;">
+          <img src="/assets/img/Artifakt-Logo.svg" style="width:140px;">
         </div>
         
         <div class="row">
           <div class="col-md-6 pt-4">
             <p>
-              <h1>Congratulations !</h1><br />
+              <h1>Congratulations, your app has been deployed !</h1><br />
               Your php application is now ready on Artifakt. Use links below to get more information.
             </p>
-            <a href="https://docs.artifakt.com" class="btn secondary">Documentation</a> 
-            <a href="https://artifakt-help.stonly.com/" class="btn secondary">Help center</a> 
+            <a href="https://docs.artifakt.com" target="_blank" class="btn secondary">Documentation</a> 
+            <a href="https://artifakt-help.stonly.com/" target="_blank"  class="btn secondary">Help center</a> 
           </div>
           <div class="col-md-6">
-            <img src="assets/img/Artifakt-picture1.png" class="img-fluid">
+            <img src="/assets/img/Artifakt-picture1.png" class="img-fluid">
           </div>
         </div>
       </div>
